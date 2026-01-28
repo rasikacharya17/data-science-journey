69b7cdadcc9d06beb0098b98924e1623176e1040
rasikacharya17 <rasikacharya171@gmail.com>
2026-01-28 23:13:30 +0545

    I changed the print statement twice

--- first.py
+++ first.py
@@ -2,3 +2,4 @@ name = input("Enter your name: ")
 age = int(input("Enter your age: "))
 future_age = age + 5
 print("Hello", name + "!", "In 5 years you will be", future_age)
+print("I removed the previous statement.")

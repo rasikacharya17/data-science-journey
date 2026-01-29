ca501d5c9174eaec9affb1a349701c1f7e79b200
rasikacharya17 <rasikacharya171@gmail.com>
2026-01-29 22:24:19 +0545

    Python basics: variables and data types

---
+++ python/Basics/variables.py
@@ -0,0 +1,16 @@
+# Variables and basic data types
+
+name = "Rasik"
+age = 22
+height = 167.5
+is_student = True
+
+print(name)
+print(age)
+print(height)
+print(is_student)
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
